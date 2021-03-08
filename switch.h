402a950278d54918cdccc771fe8b165272ba2823
Filippo Guerranti <l4plac3@gmail.com>
2021-03-08 10:58:48 +0100

    Remove GetValue() prototype and add SetValue() prototype

--- switch.h
+++ switch.h
@@ -17,4 +17,4 @@ vartable* thead; /* head of variable table list */
 /******** function prototypes ********/
 vartable* GetVar(char*);
 vartable* PutVar(char*);
-int GetValue(char*);
+void SetValue(vartable*, int);
